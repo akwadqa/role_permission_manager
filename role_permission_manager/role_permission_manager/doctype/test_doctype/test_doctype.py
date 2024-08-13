907b5414484a2dd792e97a05ac5a682393c4d832
Alaa Hariri <alaa.hariri@outlook.com>
2024-08-13 18:13:16 +0300

    Delete role_permission_manager/role_permission_manager/doctype/test_doctype directory

--- role_permission_manager/role_permission_manager/doctype/test_doctype/test_doctype.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Akwad Programming and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class testdoctype(Document):
-	pass
